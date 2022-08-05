6651b2b56635393e4d64960e624d66c511cdf41f
Lb Madesia <akarsha@aresourcepool.com>
2022-08-05 21:33:24 +0530

    set format and details.

--- routeAccess.js
+++ routeAccess.js
@@ -1,28 +1,28 @@
 const fs = require('fs').promises;
 
-    // start route access
+// start route access
 const dynamicroute = {};
-dynamicroute.routeAccess = (app,routePath,rpp) => {
-        return new Promise(async(resolve,reject)=>{
-        try{
-            const file = await fs.readdir('.'+rpp+routePath);
-            for(let i=0;i<file.length;++i){
-                const checker = await fs.lstat('.'+rpp+routePath+'/'+file[i]);
-                if(checker.isFile()){
-                    console.log('file = '+file[i],'index = '+i);
-                    let dpath = require('../..'+routePath+'/'+file[i]);
-                    app.use('/admin',dpath);
+dynamicroute.routeAccess = (app, path) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const file = await fs.readdir('./' + path);
+            for (let i = 0; i < file.length; ++i) {
+                const checker = await fs.lstat('./' + path + '/' + file[i]);
+                if (checker.isFile()) {
+                    console.log('file = ' + file[i], 'index = ' + i);
+                    let fmodule = require('../../' + path + '/' + file[i]);
+                    app.use('/admin', fmodule);
                 }
-                else{
-                    console.log('folder = '+file[i],'index = '+i);
-                    await module.exports.routeAccess(app,routePath+'/'+file[i],rpp);
+                else {
+                    console.log('folder = ' + file[i], 'index = ' + i);
+                    await module.exports.routeAccess(app, path + '/' + file[i]);
                 }
-              };
-              resolve(true);
-            }catch(error){
-                reject(error);
-            }
-        });
+            };
+            resolve(true);
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
 
-module.exports.routeAccess = dynamicroute.routeAccess;
+module.exports.access = dynamicroute.routeAccess;
